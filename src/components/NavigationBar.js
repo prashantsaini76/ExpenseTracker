770edbaffadca9ba4ffe7ab66074c6070773dab3
prashantsaini76 <prashantsaini76@gmail.com>
2024-10-09 18:21:30 +0530

    scroll fixes

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -21,37 +21,32 @@ const NavigationBar = ({expenses}) => {
 
   return (
     <nav className="bg-[#212a31] p-4 mt-4 w-[90%] mx-auto mb-8 text-white shadow-lg rounded-lg">
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container mx-auto flex justify-center items-center">
         {/* Left Section: Title */}
-        <div className="text-white text-lg font-bold">
-          Expense Manager
-        </div>
+       
 
         {/* Middle Section: Navigation Links */}
-        <div className="hidden lg:flex lg:space-x-6">
-        <button className="p-2 rounded-lg shadow-xl bg-[#1c4959]"   onClick={() => navigate("/add-expense")}>
+        <div className="flex lg:flex gap-1 lg:space-x-6">
+        <button className="p-1 sm:p-2 rounded-lg shadow-xl bg-[#1c4959]"   onClick={() => navigate("/add-expense")}>
             <div className="flex items-center gap-1">
               <IoAddCircle />
               <div className="sm:text-[12px] text-[10px]">Add Expenses</div>
             </div>
           </button>
-          <button className="p-2 rounded-lg shadow-xl bg-[#1c4959]"  onClick={() => navigate("/monthly-analysis")} >
+          <button className="p-1 sm:p-2 rounded-lg shadow-xl bg-[#1c4959] "  onClick={() => navigate("/monthly-analysis")} >
             <div className="flex items-center gap-1">
               <VscGraph />
               <div className="sm:text-[12px] text-[10px]">Monthly Analysis</div>
             </div>
           </button>
-          <button className="p-2 rounded-lg shadow-xl bg-[#1c4959]">
+          <button className="p-1 sm:p-2 rounded-lg shadow-xl bg-[#1c4959]">
             <div className="flex items-center gap-1">
               <ExportToExcel tableData={expenses} />
              
             </div>
           </button>
-        </div>
 
-        {/* Right Section: Logout */}
-        <div className="hidden lg:block">
-        <button className="p-2 rounded-lg shadow-xl bg-[#921f1f]" onClick={handleLogout}>
+          <button className="p-1 sm:p-2 rounded-lg shadow-xl bg-[#921f1f]" onClick={handleLogout}>
             <div className="flex items-center gap-1">
               <GrPowerShutdown />
               <div className="sm:text-[12px] text-[10px]">Logout</div>
@@ -59,61 +54,8 @@ const NavigationBar = ({expenses}) => {
           </button>
         </div>
 
-        {/* Mobile Menu Toggle Button */}
-        <div className="lg:hidden">
-          <button
-            onClick={toggleMenu}
-            className="text-white focus:outline-none"
-          >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16m-7 6h7'}
-              ></path>
-            </svg>
-          </button>
+       
         </div>
-      </div>
-
-      {/* Mobile Menu Links */}
-      <div className={`lg:hidden ${isOpen ? 'block' : 'hidden'}`}>
-        <div className="space-y-4 mt-4">
-         
-        <button className="p-2 rounded-lg block"   onClick={() => navigate("/add-expense")}>
-            <div className="flex items-center gap-1">
-              <IoAddCircle />
-              <div className="sm:text-[12px] text-[10px]">Add Expenses</div>
-            </div>
-          </button>
-
-         <button className="p-2 rounded-lg block"  onClick={() => navigate("/monthly-analysis")} >
-            <div className="flex items-center gap-1">
-              <VscGraph />
-              <div className="sm:text-[12px] text-[10px]">Monthly Analysis</div>
-            </div>
-          </button>
-          <button className="p-2 rounded-lg block">
-            <div className="flex items-center gap-1">
-              <ExportToExcel tableData={expenses} />
-            
-            </div>
-          </button>
-         <button className="p-2 rounded-lg block" onClick={handleLogout}>
-				<div className="flex items-center gap-1">
-				  <GrPowerShutdown />
-				  <div className="sm:text-[12px] text-[10px]">Logout</div>
-				</div>
-			  </button>
-        </div>
-      </div>
     </nav>
   );
 };
